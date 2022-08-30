e577b6e94ec21a256827d6d67011f9a547228c9e
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-08-30 20:25:00 +0100

    Fixing header test

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import { Header } from "./Header";
+import { BrowserRouter } from "react-router-dom";
 
 test("the header renders with the correct text", () => {
   const headerText = "Test Header";
 
-  render(<Header headerText={headerText} />);
+  render(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Header headerText={headerText} />
+    </BrowserRouter>
+  );
 
   const header = screen.getByText(headerText);
   expect(header).toBeInTheDocument();
