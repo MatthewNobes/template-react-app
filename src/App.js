19d0927491ce942db00efe7c723c307719f0fce7
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-08-30 18:06:23 +0100

    #7 refactoring Routes block

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ import Navigation from "./Components/Navigation";
 import FeatureOne from "./features/FeatureOne";
 import FeatureTwo from "./features/FeatureTwo";
 
+const RoutesBlock = () => (
+  <Routes>
+    <Route path="/FeatureTwo" element={<FeatureTwo />} />
+    <Route path="/" element={<FeatureOne />} />
+  </Routes>
+);
+
 const App = () => (
   <div className="App">
     <header>
       <Header headerText={"Insert Header Here"} />
     </header>
     <Navigation />
-    <Routes>
-      <Route path="/FeatureTwo" element={<FeatureTwo />} />
-      <Route path="/" element={<FeatureOne />} />
-    </Routes>
+    <RoutesBlock />
   </div>
 );
 
